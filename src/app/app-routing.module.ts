ae00e5b6581bd7f53ae294065b71fd054f4d84b5
Leonardo <leo.sopranzetti@hotmail.com>
2024-03-08 17:37:33 -0300

    fix input number

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {path: '', pathMatch: 'full', redirectTo: 'public'},
   {path: 'public', loadChildren:()=> import('./public/public.module').then((m)=>m.PublicModule)},
+  { path: '**', redirectTo: 'public' } 
 ];
 
 @NgModule({
